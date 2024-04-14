260c4b709fa0bfc64e02de5ab8bf4a2390d19bf7
Duck <juandavidp1127@gmail.com>
2024-04-14 11:59:01 -0500

    feat medicalHistory => on progress

--- src/utils/Routes.routes.js
+++ src/utils/Routes.routes.js
@@ -5,7 +5,7 @@ export const navigation = [
     { name: 'Nosotros', href: '/nosotros', current: false },
     { name: 'Citas', href: '/citas', current: false },
     { name: 'Servicios', href: '/servicios', current: false },
-    { name: 'Resultados Clinicos', href: '#', current: false },
+    { name: 'Resultados Clinicos', href: '/historiaclinica', current: false },
 ]
 /**
  * Object that contains the routes on the sideBar operator
--- src/views/UserView/MedicalHistory.jsx
+++ src/views/UserView/MedicalHistory.jsx
@@ -1,3 +1,7 @@
+import { Link } from "react-router-dom"
+import Footer from "../../components/Footer"
+import NavBar from "../../components/NavBar"
+
 /**
  * This section contains the medical records
  * @returns {Component} MedicalHistory
@@ -5,6 +9,25 @@
 const MedicalHistory = () => {
     return (
         <>
+            <NavBar />
+            <main className="h-screen flex items-center justify-center">
+                <div className="flex flex-col items-center justify-center text-2xl p-20 text-center">
+                    <h1 className="font-bold">Sorry</h1>
+                    <h2 className="font-bold" >
+                        We still working on it
+                    </h2>
+
+                    <p className="mt-4 ">Para consultar su historial medico dirigase a uno de nuestros lugares fisicos !</p>
+                    <Link to={'/'}>
+                        <button class="w-[150px] bg-black h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-primary-blue before:to-secondary-blue before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]">
+                            Atras
+                        </button>
+                    </Link>
+
+                </div>
+            </main>
+
+            <Footer />
         </>
     )
 }
