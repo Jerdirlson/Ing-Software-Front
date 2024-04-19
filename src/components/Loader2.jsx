f916256b2b25d8f82c180487d2f13e7190a6359e
Duck <118633132+Dxckrr@users.noreply.github.com>
2024-04-19 16:14:12 -0500

    fix animate on loader2

--- src/components/Loader2.jsx
+++ src/components/Loader2.jsx
@@ -2,9 +2,9 @@ const Loader2 = () => {
     return (
         <>
             <div class="flex flex-row gap-2">
-                <div class="w-4 h-4 rounded-full bg-primary-blue animate-bounce"></div>
-                <div class="w-4 h-4 rounded-full bg-secondary-blue animate-bounce [animation-delay:-.3s]"></div>
-                <div class="w-4 h-4 rounded-full bg-primary-blue animate-bounce [animation-delay:-.5s]"></div>
+                <div class="w-4 h-4 rounded-full bg-primary-blue animate-bouncing animate-iteration-count-infinite"></div>
+                <div class="w-4 h-4 rounded-full bg-secondary-blue animate-bouncing animate-delay-300 animate-iteration-count-infinite"></div>
+                <div class="w-4 h-4 rounded-full bg-primary-blue animate-bouncing animate-delay-500 animate-iteration-count-infinite"></div>
             </div>
         </>
 
